4892ed98769d2ece9a5760ff8e7bc6e16d41ac88
Konstantinov <misha.tank.2014@mail.ru>
2020-09-08 23:00:33 +0400

    создал класс точек

---
+++ src/main/java/ru/ssau/tk/cky4ajlkuh/test3/classPoint.java
@@ -0,0 +1,22 @@
+package ru.ssau.tk.cky4ajlkuh.test3;
+
+public class classPoint
+{
+    public final double x;
+    public final double y;
+    public final double z;
+
+    public classPoint(double x, double y,double z)
+    {
+        this.x=x;
+        this.y=y;
+        this.z=z;
+    }
+
+    public void Points()
+    {
+        System.out.print(x);
+        System.out.print(y);
+        System.out.print(z);
+    }
+}
